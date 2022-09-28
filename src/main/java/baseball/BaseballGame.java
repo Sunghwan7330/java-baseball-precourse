6ff24dc391e525fe1c1ddf9636d89a109ce7f40a
Sunghwan <chak1212@naver.com>
2022-09-28 22:02:07 +0900

    BaseballGame 숫자 직접입력 메서드 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,6 +1,9 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BaseballGame {
     private int mNum[];
@@ -20,10 +23,43 @@ public class BaseballGame {
         return true;
     }
 
+    private int parseStr2Int(String strnum) {
+        int inputNum;
+        try {
+            inputNum = Integer.parseInt(strnum);
+        } catch (Exception e) {
+            String errStr = "숫자가 아닌 값이 입력되었습니다 -> " + strnum;
+            throw new IllegalArgumentException(errStr);
+        }
+        return inputNum;
+    }
+
+    private boolean isVaildNumber(int number) {
+        //TODO 입력값 범위체크 로직 추가 필요
+        return true;
+    }
+
+    private boolean setMemberNumber(int number) {
+        mNum[0] = number / 100;
+        number %= 100;
+        mNum[1] = number / 10;
+        number %= 10;
+        mNum[2] = number;
+
+        return true;
+    }
+
+    public boolean setNumber(String strnum) {
+        int inputNum = parseStr2Int(strnum);
+        isVaildNumber(inputNum);
+        setMemberNumber(inputNum);
+
+        return true;
+    }
+
     public void printNum() {
         for (int i = 0; i < mNum.length; i++)
             System.out.print(mNum[i]);
         System.out.println();
     }
-
 }
--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -4,6 +4,7 @@ package baseball;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BaseballGameTest {
 
@@ -20,5 +21,30 @@ public class BaseballGameTest {
         assertThat(game.getNum())
                 .isNotEqualTo(new int[3])
                 .doesNotContain(0);
+    }
+
+    @Test
+    public void baseballGameSetNumBlankTest() {
+        BaseballGame game = new BaseballGame();
+        assertThatThrownBy(() -> game.setNumber(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자가 아닌 값이 입력되었습니다 -> ");
+    }
+
+    @Test
+    public void baseballGameSetNumStringTest() {
+        BaseballGame game = new BaseballGame();
+        assertThatThrownBy(() -> game.setNumber("aaa"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자가 아닌 값이 입력되었습니다 -> aaa");
+    }
+
+    @Test
+    public void baseballGameSetNum123Test() {
+        BaseballGame game = new BaseballGame();
+        assertThat(game.setNumber("123"))
+                .isEqualTo(true);
+        assertThat(game.getNum())
+                .isEqualTo(new int[]{1, 2, 3});
     }
 }
